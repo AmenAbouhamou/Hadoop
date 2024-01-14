6c236f463e118868ff89891a7d2333cac2b2dd27
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2024-01-14 21:17:39 +0100

    Update execute.sh

--- hadoop/DevHadoop/execute.sh
+++ hadoop/DevHadoop/execute.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 FIC=$1
+pattern="[a-zA-Z]+[0-9]+?\.csv" #pattern fichier.csv
+pattern2="[all]" # pattern pour selectionner tous les fichiers
+
 hadoop com.sun.tools.javac.Main Select*.java
 jar cf select.jar Select*.class
 hadoop fs -rm -r -f /project/output*
-hadoop jar select.jar Select /project/input/$FIC /project/output
+
+if [[ "$FIC" =~ $pattern ]]; then
+  hadoop jar select.jar Select /project/input/$FIC /project/output
+elif [[ "$FIC" =~ $pattern2 ]]; then
+  hadoop jar select.jar Select /project/input /project/output
+fi
+
 hadoop fs -cat /project/output/part-r-00000
