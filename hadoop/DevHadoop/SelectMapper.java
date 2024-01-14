cd5356de3bb6bdd93181ad045d8446ece22ce07b
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2024-01-14 16:23:22 +0100

    Update SelectMapper.java

--- hadoop/DevHadoop/SelectMapper.java
+++ hadoop/DevHadoop/SelectMapper.java
@@ -12,16 +12,10 @@ public class SelectMapper extends Mapper<LongWritable, Text, LongWritable, Text>
     private LongWritable count=new LongWritable(0);
     private ArrayList<String> arrays= new ArrayList<>();
     public void map(LongWritable key, Text value,Context context)throws IOException, InterruptedException {
-        if(!arrays.isEmpty()){
             if(arrays.stream().noneMatch(s -> s.equals(value.toString()))){
                 arrays.add(value.toString());
                 context.write(count,value);
                 count.set(count.get()+1);
-            } 
-        }else {
-            context.write(count, value);
-            count.set(count.get() + 1);
-            
-        }
+            }
     }
 }
