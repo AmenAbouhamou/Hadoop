e267554b6d6b57fba571052dc572c40081592f1c
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2024-01-14 16:26:22 +0100

    Create execute.sh

---
+++ hadoop/DevHadoop/execute.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FIC=$1
+hadoop com.sun.tools.javac.Main Select*.java
+jar cf select.jar Select*.class
+hadoop fs -rm -r -f /project/output*
+hadoop jar select.jar Select /project/input/$FIC /project/output
+hadoop fs -cat /project/output/part-r-00000
