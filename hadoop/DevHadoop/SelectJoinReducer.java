90cce19063c0750289ef9dba5bde41f90cc7a4e3
AmenAbouhamou <52853649+AmenAbouhamou@users.noreply.github.com>
2024-01-15 17:23:09 +0100

    Create SelectJoinReducer.java

---
+++ hadoop/DevHadoop/SelectJoinReducer.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.Text;
+
+public class SelectJoinReducer extends Reducer<LongWritable, Text,LongWritable, Text>{
+    //(Select distinct * from Employees,Lieu_Travail where Employe.idtravail=Lieu_Travail.id
+    private LongWritable count=new LongWritable(0);
+    private ArrayList<String> arrays= new ArrayList<>(),
+            lieu= new ArrayList<>(),
+            user= new ArrayList<>();
+    public void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException,InterruptedException {
+        for (Text value:values){
+            String[] att = value.toString().split(",");
+            if (arrays.stream().noneMatch(s -> s.equals(value.toString())) && att.length > 2) {
+                arrays.add(value.toString());
+            }
+            if (lieu.stream().noneMatch(s -> s.equals(value.toString())) && att.length == 2) {
+                lieu.add(value.toString());
+            }
+        }
+        for (String userval:arrays) {
+            for (String lieuval : lieu) {
+                if (user.stream().noneMatch(s -> s.equals(userval + "," + lieuval))) {
+                    user.add(userval + "," + lieuval);
+                    String[] attrib = (userval + "," + lieuval).split(",");
+                    if (attrib[3].equals(attrib[9])) {
+                        String newVal=joinString(attrib,3,10,8);
+                        context.write(count, new Text(newVal));
+                        count.set(count.get() + 1);
+                    }
+                }
+            }
+        }
+    }
+    public String joinString(String[] a,int i1,int i2,int limit){
+        StringBuilder msg= new StringBuilder();
+        for (int i = 0; i < limit; i++) {
+            if (i!=i1)
+                msg.append(a[i]).append(",");
+            else
+                msg.append(a[i2]).append(",");
+        }
+        return msg.toString();
+    }
+}
